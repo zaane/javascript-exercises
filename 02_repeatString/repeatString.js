e46be2593e11ee017199ad0ea453720cb36f1139
zane o <zaneolds@gmail.com>
2024-03-20 00:36:52 -0500

    solve problems 1-3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return 'ERROR';
+    }
+    repeatedString = '';
+    for (i = 0; i < num; i++) {
+        repeatedString += string;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    if (string.length < 0) {
+        return 'ERROR'
+    }
+    reversedString = '';
+    for (i = string.length - 1; i>=0; i--) {
+        reversedString += string[i];
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
